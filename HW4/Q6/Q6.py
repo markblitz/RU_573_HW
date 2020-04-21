b6b9799b1af2f1d5e65362e7fb841cd30bbc951f
Yiming <blitzblq@gmail.com>
2020-04-21 17:38:03 -0400

    4.1

--- HW4/Q6/Q6.py
+++ HW4/Q6/Q6.py
@@ -7,15 +7,12 @@
 #site： https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
 
 
-# In[13]:
+# In[7]:
 
 
 # Python program for Dijkstra's single 
 # source shortest path algorithm. The program is 
 # for adjacency matrix representation of the graph 
-
-# Library for INT_MAX 
-import sys 
 
 class Graph(): 
 
@@ -77,7 +74,7 @@ class Graph():
         self.printSolution(dist) 
 
 
-# In[15]:
+# In[10]:
 
 
 Q4b = Graph(8)
@@ -99,27 +96,11 @@ matrix[3][6] = 0.52
 matrix[6][0] = 0.58
 matrix[6][4] = 0.93
 
-matrix[5][4] = 0.35
-matrix[4][5] = -0.66
-matrix[7][4] = 0.37
-matrix[7][5] = 0.28
-matrix[5][7] = 0.28
-matrix[1][5] = 0.32
-matrix[4][0] = 0.38
-matrix[2][0] = 0.26
-matrix[3][7] = 0.39
-matrix[3][1] = 0.29
-matrix[7][2] = 0.34
-matrix[2][6] = 0.40
-matrix[6][3] = 0.52
-matrix[0][6] = 0.58
-matrix[4][6] = 0.93
-
 Q4b.graph = matrix
 Q4b.dijkstra(0)
 
 
-# In[16]:
+# In[11]:
 
 
 Q4a = Graph(8)
@@ -141,24 +122,8 @@ matrix[3][6] = 0.52
 matrix[6][0] = -1.4
 matrix[6][4] = -1.25
 
-matrix[5][4] = 0.35
-matrix[4][5] = 0.35
-matrix[7][4] = 0.37
-matrix[7][5] = 0.28
-matrix[5][7] = 0.28
-matrix[1][5] = 0.32
-matrix[4][0] = 0.38
-matrix[2][0] = 0.26
-matrix[3][7] = 0.39
-matrix[3][1] = 0.29
-matrix[7][2] = 0.34
-matrix[2][6] = -1.2
-matrix[6][3] = 0.52
-matrix[0][6] = -1.4
-matrix[4][6] = -1.25
-
-Q4b.graph = matrix
-Q4b.dijkstra(0)
+Q4a.graph = matrix
+Q4a.dijkstra(0)
 
 
 # In[ ]:
